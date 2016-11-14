47a8a7a09667e4e3c0fbe287e4eeb12b63311de4
Achyut Kulkarni <achyut.kulkarni@ff.com>
2016-11-13 19:59:20 -0800

    Photo upload multipart work

---
+++ app/src/main/java/com/nightson/MultipartUpload.java
@@ -0,0 +1,135 @@
+package com.nightson;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyLog;
+
+//import org.apache.http.entity.mime.HttpMultipartMode;
+//import org.apache.http.HttpEntity;
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by achi on 11/13/16.
+ */
+
+public class MultipartUpload extends Request<String> {
+
+//    private MultipartEntity entity = new MultipartEntity();
+
+
+    MultipartEntityBuilder entity = MultipartEntityBuilder.create();
+    HttpEntity httpentity;
+    private static final String REGISTER_REQUEST_URL = "http://vswamy.net:8888/upload";
+    private static final String FILE_PART_NAME = "file_name";
+    private static final String STRING_PART_NAME = "text";
+//    HttpEntity httpentity;
+
+    private String mStringPart;
+    private Map<String, String> params;
+    private final Response.Listener<String> mListener;
+    private  File mFilePart;
+    private Map<String, String> headers;
+
+
+    public MultipartUpload(File file,String token,  Response.Listener<String> listener, Response.ErrorListener errorlistener) {
+        super(Method.POST, REGISTER_REQUEST_URL, errorlistener);
+
+        mListener = listener;
+        mFilePart = file;
+        mStringPart = token;
+//        entity.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+        buildMultipartEntity();
+//        httpentity = entity.build();
+//        entity = new  MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+
+
+
+    }
+    private void buildMultipartEntity()
+    {
+        entity.addPart(FILE_PART_NAME, new FileBody(mFilePart));
+        try
+        {
+            entity.addPart(STRING_PART_NAME, new StringBody(""));
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            VolleyLog.e("UnsupportedEncodingException");
+        }
+    }
+
+
+    @Override
+    public String getBodyContentType()
+    {
+        return httpentity.getContentType().getValue();
+//        return entity
+    }
+
+    @Override
+    public byte[] getBody() throws AuthFailureError
+    {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try
+        {
+            httpentity = entity.build();
+            httpentity.writeTo(bos);
+//            entity.writeTo(bos);
+//            httpentity.httpentitywriteTo(bos);
+        }
+        catch (IOException e)
+        {
+            VolleyLog.e("IOException writing to ByteArrayOutputStream");
+        }
+        return bos.toByteArray();
+    }
+
+    @Override
+    public Map<String, String> getHeaders() throws AuthFailureError {
+        Map<String, String> headers = super.getHeaders();
+
+        if (headers == null
+                || headers.equals(Collections.emptyMap())) {
+            headers = new HashMap<String, String>();
+        }
+
+        headers.put("Content-Type", "multipart/form-data");
+        headers.put("x-auth-token",mStringPart);
+
+
+        return headers;
+    }
+    @Override
+    protected Response<String> parseNetworkResponse(NetworkResponse response) {
+        return Response.success("Uploaded", getCacheEntry());
+    }
+
+    @Override
+    protected void deliverResponse(String response) {
+        mListener.onResponse(response);
+    }
+
+//
+//    public static interface MultipartProgressListener {
+//        void transferred(long transfered, int progress);
+//    }
+
+
+}
--- app/src/main/java/com/nightson/UploadImage.java
+++ app/src/main/java/com/nightson/UploadImage.java
@@ -1,82 +1,190 @@
 package com.nightson;
 
-import android.content.Context;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Map;
 
-import static com.nightson.R.id.imageView;
-
-public class UploadImage extends AppCompatActivity {
+public class UploadImage extends AppCompatActivity implements View.OnClickListener{
     private ImageView imageView;
-    private final int SELECT_PHOTO = 1;
+    private Button browseBtn;
+    private Button uploadBtn;
+    private Bitmap bitmap;
+    private final int PICK_IMAGE_REQUEST = 1;
+    private String UPLOAD_URL ="http://vswamy.net:8888/upload";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_image2);
         imageView = (ImageView) findViewById(R.id.viewImage);
+        browseBtn = (Button) findViewById(R.id.browseBtn);
+        uploadBtn = (Button) findViewById(R.id.uploadBtn);
+        browseBtn.setOnClickListener(this);
+        uploadBtn.setOnClickListener(this);
 
-        Button pickImage = (Button) findViewById(R.id.upload);
-        pickImage.setOnClickListener(new View.OnClickListener() {
+    }
+
+    public String getStringImage(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+
+    public File getFileImage(Bitmap bmp)  {
+        File f = new File(getCacheDir(), "upload_image");
+        try {
+            f.createNewFile();
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] imageBytes = baos.toByteArray();
+//        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+            FileOutputStream fos = new FileOutputStream(f);
+            fos.write(imageBytes);
+            fos.flush();
+            fos.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return f;
+    }
+
+    private void uploadImage()  {
+        //Showing the progress dialog
+        SharedPreferences preferences = getSharedPreferences(Constants.PREF_FILE_NAME,0);
+        final String token = preferences.getString("x-auth-token","None");
+        final ProgressDialog loading = ProgressDialog.show(this,"Uploading...","Please wait...",false,false);
+
+        Response.Listener<String> responseListener = new Response.Listener<String>(){
+            @Override
+            public void onResponse(String s) {
+                //Disimissing the progress dialog
+                loading.dismiss();
+                Log.d("response", s);
+                //Showing toast message of the response
+                Toast.makeText(UploadImage.this, s , Toast.LENGTH_LONG).show();
+            }
+        };
+
+        Response.ErrorListener ErrorresponseListener = new Response.ErrorListener()
+        {
 
             @Override
-            public void onClick(View view) {
-                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                photoPickerIntent.setType("image/*");
-                startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+            public void onErrorResponse(VolleyError error)
+            {
+
+//                            Intent intent = new Intent(PasswordChange.this,
+//                            MapActivity.class);
+//                            PasswordChange.this.startActivity(intent);
+                Log.d("Error", String.valueOf(error));
+
             }
-        });
+
+        };
+//
+//{
+//            @Override
+//            protected Map<String,File> () throws AuthFailureError {
+//                //Converting Bitmap to String
+////                String image = getStringImage(bitmap);
+//                try {
+                    File file = getFileImage(bitmap);
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                //Creating parameters
+//                Map<String,File> params = new Hashtable<String, File>();
+//                //Adding parameters
+//                params.put("file_name", file);
+//                //returning parameters
+//                return params;
+//            }
+
+//            @Override
+//            public Map<String, String> getHeaders()
+//
+//            {
+//                Map<String,String> headers = new Hashtable<String, String>();
+//                headers.put("x-auth-token",token);
+//                return headers;
+//            }
+        MultipartUpload UploadRequest = new MultipartUpload(
+                file , token, responseListener,ErrorresponseListener );
+
+        VolleyHelper.getInstance(getApplicationContext());
+        VolleyHelper vh = VolleyHelper
+                .getInstance(getApplicationContext());
+        vh.getRequestQueue().add(UploadRequest);
+
 
     }
-
-    protected void next() {
-        Intent registerIntent = new Intent(UploadImage.this,
-                MapActivity.class);
-        startActivity(registerIntent);
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
     }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
-            super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-            switch(requestCode) {
-                case SELECT_PHOTO:
-                    if(resultCode == RESULT_OK){
-                        try {
-                            final Uri imageUri = imageReturnedIntent.getData();
-                            final InputStream imageStream = getContentResolver().openInputStream(imageUri);
-                            final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
-                            imageView.setImageBitmap(selectedImage);
-                            next();
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            try {
+                //Getting the Bitmap from Gallery
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                //Setting the Bitmap to ImageView
+                imageView.setImageBitmap(bitmap);
 
-                        } catch (FileNotFoundException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return true;
     }
 
 
+
+
+
+    @Override
+    public void onClick(View v) {
+        if(v == browseBtn){
+            showFileChooser();
+        }
+
+        if(v == uploadBtn){
+            uploadImage();
+        }
+    }
 }
