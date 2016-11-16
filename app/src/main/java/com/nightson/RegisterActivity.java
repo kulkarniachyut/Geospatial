8245eb881ec65199e7a635ec2c94860c01b59fab
kulkarniachyut <achyut.gk@gmail.com>
2016-11-15 23:06:17 -0800

    Changed manifest file

--- app/src/main/java/com/nightson/RegisterActivity.java
+++ app/src/main/java/com/nightson/RegisterActivity.java
@@ -2,9 +2,11 @@ package com.nightson;
 
 import android.content.Intent;
 import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,15 +22,13 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
 public class RegisterActivity extends AppCompatActivity
-        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
-{
+        implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     private GoogleApiClient mGoogleApiClient;
     public static final String TAG = LoginActivity.class.getSimpleName();
     private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
@@ -46,18 +46,26 @@ public class RegisterActivity extends AppCompatActivity
         Log.d("reaches here 2!", "maybe2");
         Button registerBtn = (Button) findViewById(R.id.btnRegister);
 
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         final Location location = LocationServices.FusedLocationApi
                 .getLastLocation(mGoogleApiClient);
 
         Log.d("location", String.valueOf(location));
         Log.d("location", "?");
 
-        registerBtn.setOnClickListener(new View.OnClickListener()
-        {
+        registerBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 Log.d("working", "1");
 
                 final String first_name = fname.getText().toString();
@@ -70,12 +78,10 @@ public class RegisterActivity extends AppCompatActivity
                 Log.d("try 2 location", String.valueOf(location));
                 Log.d("location", "?");
 
-                Response.Listener<String> responseListener = new Response.Listener<String>()
-                {
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
 
                     @Override
-                    public void onResponse(String response)
-                    {
+                    public void onResponse(String response) {
                         Intent intent = new Intent(RegisterActivity.this,
                                 LoginActivity.class);
                         intent.putExtra("email", email);
@@ -89,12 +95,10 @@ public class RegisterActivity extends AppCompatActivity
 
                 };
 
-                Response.ErrorListener ErrorresponseListener = new Response.ErrorListener()
-                {
+                Response.ErrorListener ErrorresponseListener = new Response.ErrorListener() {
 
                     @Override
-                    public void onErrorResponse(VolleyError error)
-                    {
+                    public void onErrorResponse(VolleyError error) {
 
                         Intent intent = new Intent(RegisterActivity.this,
                                 LoginActivity.class);
@@ -118,8 +122,7 @@ public class RegisterActivity extends AppCompatActivity
 
     }
 
-    private void handleNewLocation(Location location)
-    {
+    private void handleNewLocation(Location location) {
         Log.d(TAG, location.toString());
         double currentLatitude = location.getLatitude();
         double currentLongitude = location.getLongitude();
@@ -130,9 +133,18 @@ public class RegisterActivity extends AppCompatActivity
     @Override
     public void onConnected(
             @Nullable
-                    Bundle bundle)
-    {
+                    Bundle bundle) {
         Log.i(TAG, "Location services connected.");
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         Location location = LocationServices.FusedLocationApi
                 .getLastLocation(mGoogleApiClient);
         if (location == null)
