97fe14fa42524303ce7a1382aeb7ad19b31b4098
kulkarniachyut <achyut.gk@gmail.com>
2016-11-15 22:42:32 -0800

    please work

--- app/src/main/java/com/nightson/MapFragmentNew.java
+++ app/src/main/java/com/nightson/MapFragmentNew.java
@@ -140,7 +140,10 @@ public class MapFragmentNew extends Fragment
                         markerOptions.position(latLng);
                         markerOptions.title(party_name);
 
-                        mMap.addMarker(markerOptions);
+                        Marker x = mMap.addMarker(markerOptions);
+
+
+                        map.put(x, jsonObject);
                     }
                 }
                 catch (JSONException e)
@@ -205,10 +208,24 @@ public class MapFragmentNew extends Fragment
 
     }
 
-
+    HashMap<Marker, JSONObject> map = new HashMap<Marker, JSONObject>();
     public boolean onMarkerClick(Marker marker) {
-        marker.hideInfoWindow();
-        showPopup(getActivity(),marker);
+//        marker.hideInfoWindow();
+        JSONObject jsob  = map.get(marker);
+        Intent i = new Intent(getActivity(), RsvpDescActivity.class);
+        try {
+            i.putExtra("name" , jsob.getString("name"));
+            i.putExtra("starttime" , jsob.getString("start_time"));
+            i.putExtra("endtime" , jsob.getString("end_time"));
+            i.putExtra("rsvp" , jsob.getInt("rsvp"));
+            i.putExtra("id" , jsob.getString("id"));
+//            i.putExtra("name" , jsob.getString("name"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        startActivity(i);
+//        showPopup(getActivity(),marker);
         return true;
     }
 
