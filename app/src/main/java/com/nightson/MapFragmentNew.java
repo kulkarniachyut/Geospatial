5890e7d3fee0d6389bc27ae069c05945109c2175
Virupaksha Swamy U M <vswamy@outlook.com>
2016-11-30 18:00:51 -0800

    fixed bugs

--- app/src/main/java/com/nightson/MapFragmentNew.java
+++ app/src/main/java/com/nightson/MapFragmentNew.java
@@ -1,13 +1,10 @@
 package com.nightson;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.graphics.Point;
-import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
+import android.location.LocationProvider;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -19,13 +16,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.TextView;
 import android.widget.Toast;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -60,55 +53,50 @@ public class MapFragmentNew extends Fragment
 
     }
 
-    @Override
-    public void onLocationChanged(Location location)
+    public void clearMap()
     {
-        mLastLocation = location;
-        if (currLocationMarker != null)
-        {
-            currLocationMarker.remove();
-        }
+        mMap.clear();
+    }
 
-        LatLng latLng = new LatLng(location.getLatitude(),
-                location.getLongitude());
+    public void addCurrentPositionMarker(LatLng latLng)
+    {
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(latLng);
         markerOptions.title("Current Position");
         markerOptions.icon(BitmapDescriptorFactory
                 .defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
         currLocationMarker = mMap.addMarker(markerOptions);
-
-        //move map camera
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
         mMap.addCircle(new CircleOptions().center(latLng)
                 .radius(Constants.DEFAULT_RADIUS).strokeWidth(0)
                 .fillColor(0x25808080));
-        mMap.setMapStyle(MapStyleOptions
-                .loadRawResourceStyle(getActivity().getBaseContext(),
-                        R.raw.ub_map));
         mMap.setOnMarkerClickListener(this);
-
-        //stop location updates
-        if (mGoogleApiClient != null)
-        {
-            LocationServices.FusedLocationApi
-                    .removeLocationUpdates(mGoogleApiClient, this);
-        }
-        loadnearbyparties(location, mMap.getCameraPosition().zoom);
-
     }
 
-    private void loadnearbyparties(Location location, float zoomlev)
+    @Override
+    public void onLocationChanged(Location location)
     {
-        double lat = location.getLatitude();
-        double lng = location.getLongitude();
+        clearMap();
+        LatLng latLng = new LatLng(location.getLatitude(),
+                location.getLongitude());
+        addCurrentPositionMarker(latLng);
+        loadnearbyparties(latLng);
+    }
+
+    private void loadnearbyparties(LatLng latlng)
+    {
+        float zoomlev = mMap.getCameraPosition().zoom;
+        double lat = latlng.latitude;
+        double lng = latlng.longitude;
         double radius = Math.exp((16 - zoomlev) * Math.log(2)) * 500;
         // volley code
         String url =
                 "http://vswamy.net:8888/search?latitude=" + lat + "&longitude="
                         + lng + "&radius=2000";
-        SharedPreferences preferences = getActivity()
+        if(super.getContext() == null)
+            return;
+        SharedPreferences preferences = super.getContext()
                 .getSharedPreferences(Constants.PREF_FILE_NAME, 0);
         final String token = preferences.getString("x-auth-token", "None");
 
@@ -181,33 +169,9 @@ public class MapFragmentNew extends Fragment
     @Override
     public void onMapClick(LatLng latLng)
     {
-
-        MarkerOptions addressmarker = new MarkerOptions();
-
-        addressmarker.position(latLng);
-        addressmarker.title(latLng.latitude + " : " + latLng.longitude);
-
-        Location temploc = new Location("");
-        temploc.setLatitude(currLocationMarker.getPosition().latitude);
-        temploc.setLongitude(currLocationMarker.getPosition().longitude);
-        LatLng ll = new LatLng(temploc.getLatitude(), temploc.getLongitude());
-        mMap.clear();
-        // add current location marker after clearing
-        MarkerOptions curloc = new MarkerOptions();
-        curloc.position(ll);
-        curloc.title("Current Location");
-        curloc.icon(BitmapDescriptorFactory
-                .defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
-        mMap.addMarker(curloc);
-
-        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-        addressmarker.icon(BitmapDescriptorFactory
-                .defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
-        Marker currmarker = mMap.addMarker(addressmarker);
-        Location location = new Location("");
-        location.setLatitude(currmarker.getPosition().latitude);
-        location.setLongitude(currmarker.getPosition().longitude);
-        loadnearbyparties(location, mMap.getCameraPosition().zoom);
+        clearMap();
+        addCurrentPositionMarker(latLng);
+        loadnearbyparties(latLng);
     }
 
     public boolean onMarkerClick(Marker marker)
@@ -246,17 +210,14 @@ public class MapFragmentNew extends Fragment
     public void onRequestPermissionsResult(int requestCode,
             String permissions[], int[] grantResults)
     {
-        //  Toast.makeText(this, requestCode, Toast.LENGTH_LONG).show();
         switch (requestCode)
         {
             case MY_PERMISSIONS_REQUEST_LOCATION:
             {
-                // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 {
 
-                    // Permission was granted.
                     if (ContextCompat.checkSelfPermission(getActivity(),
                             android.Manifest.permission.ACCESS_FINE_LOCATION)
                             == PackageManager.PERMISSION_GRANTED)
@@ -272,56 +233,21 @@ public class MapFragmentNew extends Fragment
                 }
                 else
                 {
-
-                    // Permission denied, Disable the functionality that depends on this permission.
                     Toast.makeText(getActivity().getBaseContext(),
                             "permission denied", Toast.LENGTH_LONG).show();
                 }
                 return;
             }
-
-            // other 'case' lines to check for other permissions this app might request.
-            //You can add here other case statements according to your requirement.
         }
     }
 
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
-    public boolean checkLocationPermission()
-    {
-        if (ContextCompat.checkSelfPermission(getActivity().getBaseContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED)
-        {
-
-            if (ActivityCompat
-                    .shouldShowRequestPermissionRationale(getActivity(),
-                            android.Manifest.permission.ACCESS_FINE_LOCATION))
-            {
-
-                ActivityCompat.requestPermissions(getActivity(),
-                        new String[] { android.Manifest.permission.ACCESS_FINE_LOCATION },
-                        MY_PERMISSIONS_REQUEST_LOCATION);
-
-            }
-            else
-            {
-                ActivityCompat.requestPermissions(getActivity(),
-                        new String[] { android.Manifest.permission.ACCESS_FINE_LOCATION },
-                        MY_PERMISSIONS_REQUEST_LOCATION);
-            }
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState)
     {
+        super.onAttach(getActivity().getApplicationContext());
         View view = inflater.inflate(R.layout.fragment_map, container, false);
         FragmentManager fm = getChildFragmentManager();
         SupportMapFragment mapFragment = (SupportMapFragment) fm
@@ -335,7 +261,6 @@ public class MapFragmentNew extends Fragment
             @Override
             public void onClick(View v)
             {
-                // TODO Auto-generated method stub
                 Intent i = new Intent(getActivity(), AddEventActivity.class);
                 startActivity(i);
 
@@ -355,21 +280,20 @@ public class MapFragmentNew extends Fragment
     @Override
     public boolean onMyLocationButtonClick()
     {
-        Location temploc = new Location("");
-        temploc.setLatitude(currLocationMarker.getPosition().latitude);
-        temploc.setLongitude(currLocationMarker.getPosition().longitude);
-        LatLng ll = new LatLng(temploc.getLatitude(), temploc.getLongitude());
-        mMap.clear();
-        // add current location marker after clearing
-        MarkerOptions curloc = new MarkerOptions();
-        curloc.position(ll);
-        curloc.title("Current Location");
-        curloc.icon(BitmapDescriptorFactory
-                .defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
-        mMap.addMarker(curloc);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(ll));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(17));
-        loadnearbyparties(temploc, 17);
+        if (ActivityCompat.checkSelfPermission(getContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat
+                .checkSelfPermission(getContext(),
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            return false;
+        }
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        clearMap();
+        addCurrentPositionMarker(latLng);
+        loadnearbyparties(latLng);
         return true;
     }
 
@@ -387,15 +311,30 @@ public class MapFragmentNew extends Fragment
             {
                 buildGoogleApiClient();
                 mMap.setMyLocationEnabled(true);
-
             }
         }
         else
         {
             buildGoogleApiClient();
             mMap.setMyLocationEnabled(true);
-
         }
+
+        mMap.setMapStyle(MapStyleOptions
+                .loadRawResourceStyle(getActivity().getBaseContext(),
+                        R.raw.ub_map));
+
+
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(location == null)
+        {
+            location = new Location("");
+            location.setLatitude(34.0224);
+            location.setLongitude(118.2851);
+        }
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        clearMap();
+        addCurrentPositionMarker(latLng);
+        loadnearbyparties(latLng);
     }
 
     @Override
@@ -403,8 +342,6 @@ public class MapFragmentNew extends Fragment
             @Nullable
                     Bundle bundle)
     {
-        Toast.makeText(getActivity().getBaseContext(), "HELLO",
-                Toast.LENGTH_LONG).show();
         LocationRequest mLocationRequest;
         mLocationRequest = LocationRequest.create();
         mLocationRequest.setInterval(1000);
